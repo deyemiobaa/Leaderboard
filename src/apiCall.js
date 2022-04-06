4df5b7b32bae2bdd0ac09109fccff958c969056b
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-04-06 22:48:27 +0100

    Call api to add and get scores

---
+++ src/apiCall.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+const gameId = 'fI1tffmB55k9l8dBQbMp';
+
+export const addScore = async (name, score) => {
+  await fetch(`${BASE_URL}${gameId}/scores`, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: name,
+      score: score,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then(res => res.json())
+}
+
+export const getScore = async () => {
+  let data
+  await fetch(`${BASE_URL}${gameId}/scores`).then(res => res.json()).then(res => data = res)
+  return data
+}
